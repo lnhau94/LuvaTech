85193e974b9ba2fbe9c10a6bc4e034bac1bced93
hau <lnhau94@gmail.com>
2022-11-20 18:43:40 +0700

    edit pom file for first set up

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,8 +1,35 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class Hello {
+public class Hello extends Application {
+
     public static void main(String[] args) {
-        System.out.println("Hello babe");
+        launch(args);
 
 
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        stage.setScene(new Scene(root, 300, 250));
+        stage.show();
     }
 }
