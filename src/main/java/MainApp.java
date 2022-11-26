072d901f5a48bc03869055bb3cfd67341091c2f7
hau <lnhau94@gmail.com>
2022-11-26 19:14:14 +0700

    add switch scene function

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,58 @@
+import Util.Validation;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    private static Stage mainStage;
+
+    /**
+     * Switch to new Scene - Chuyển Scene
+     * @param newScene Scene cần hiển thị
+     */
+    public static void switchScene(Scene newScene){
+        mainStage.setScene(newScene);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("" + new Validation().checkPhone("+84965026920"));
+
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        mainStage = stage;
+        stage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        stage.setScene(new Scene(root, 300, 250));
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                StackPane pane = new StackPane();
+                Button btn = new Button("hello");
+                btn.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        switchScene(root.getScene());
+                    }
+                }
+
+                );
+                pane.getChildren().add(btn);
+                switchScene(new Scene(pane));
+            }
+        });
+
+
+        stage.show();
+    }
+}
