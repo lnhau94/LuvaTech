6559310e249ae81b3628d81ff525f124485949e2
daisgu014 <daibucky@gmail.com>
2022-11-27 10:08:48 +0700

    MainApp

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -32,40 +32,37 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-//        mainStage = stage;
-//        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-//        mainStage.setWidth(dim.getWidth());
-//        mainStage.setHeight(dim.getHeight());
-//
-//        AccountDAO.retrieve();
-//        ProductDAO.retrieve();
-//        stage.setTitle("LuvaTech");
-//        Button btn = new Button();
-//        btn.setText("Say 'Hello World'");
-//        StackPane root = new StackPane();
-//        root.getChildren().add(btn);
-//        stage.setScene(new Scene(root, 300, 250));
-//        btn.setOnAction(new EventHandler<ActionEvent>() {
-//
-//            @Override
-//            public void handle(ActionEvent event) {
-//                StackPane pane = new StackPane();
-//                Button btn = new Button("hello");
-//                btn.setOnAction(new EventHandler<ActionEvent>() {
-//                                    @Override
-//                                    public void handle(ActionEvent actionEvent) {
-//                                        switchScene(root.getScene());
-//                                    }
-//                                }
-//
-//                );
-//                pane.getChildren().add(btn);
-//                switchScene(new Scene(pane));
-//            }
-//        });
+        mainStage = stage;
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        mainStage.setWidth(dim.getWidth());
+        mainStage.setHeight(dim.getHeight());
 
-        stage.setScene(new Scene(FXMLLoader.load(new File("src/main/java/App/View/view-product.fxml").toURI().toURL())));
-       // System.out.println(getClass().getResource("/Users/lnhau/Documents/Code/luvatech/src/main/java/MainApp.java"));
+        AccountDAO.retrieve();
+        ProductDAO.retrieve();
+        stage.setTitle("LuvaTech");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        stage.setScene(new Scene(root, 300, 250));
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                StackPane pane = new StackPane();
+                Button btn = new Button("hello");
+                btn.setOnAction(new EventHandler<ActionEvent>() {
+                                    @Override
+                                    public void handle(ActionEvent actionEvent) {
+                                        switchScene(root.getScene());
+                                    }
+                                }
+
+                );
+                pane.getChildren().add(btn);
+                switchScene(new Scene(pane));
+            }
+        });
         stage.show();
     }
 }
