7a9f1cc1877157644de080cb78abbe620fccb362
ThanhHuyen2212 <huyennguyen850.vn@gmail.com>
2022-12-09 20:43:39 +0700

    add new purchase order and give back item DAO

--- src/main/java/DAL/MappingDAO.java
+++ src/main/java/DAL/MappingDAO.java
@@ -2,6 +2,7 @@ package DAL;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
 
 public class MappingDAO {
@@ -22,5 +23,41 @@ public class MappingDAO {
         }
 
         return orderStaff;
+    }
+
+    public static HashMap<String, String> mapOrderStaffTakeBack() {
+        HashMap<String, String> orderStaffTakeBack = new HashMap<>();
+
+        DAO dao = new DAO();
+        Statement stmt = dao.getStmt();
+        try {
+            ResultSet rs = stmt.executeQuery("select orderid, staffback from orders");
+            while(rs != null && rs.next()) {
+                orderStaffTakeBack.put(rs.getString(1), rs.getString(2));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return orderStaffTakeBack;
+    }
+
+    public static HashMap<String, String[]> mapPOStaff() {
+        HashMap<String, String[]> POStaff = new HashMap<>();
+
+        DAO dao = new DAO();
+        Statement stmt = dao.getStmt();
+        try {
+            ResultSet rs = stmt.executeQuery("select purchaseorderid, employeeidcreate, " +
+                    "employeeidconfirm from purchaseorder");
+            while(rs != null && rs.next()) {
+                POStaff.put(rs.getString(1), new String[] {
+                        rs.getString(2), rs.getString(3)});
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return POStaff;
     }
 }
