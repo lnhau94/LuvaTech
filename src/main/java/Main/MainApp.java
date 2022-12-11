3e2434af86b9f0252434e771ee80a76ea9006bdf
pndhuy150902 <phamduchuy159@gmail.com>
2022-12-11 11:14:38 +0700

    Huy Face Recognition

--- src/main/java/Main/MainApp.java
+++ src/main/java/Main/MainApp.java
@@ -29,7 +29,7 @@ public class MainApp extends Application {
      */
     public static void switchScene(Scene newScene){
         mainStage.setScene(newScene);
-        mainStage.setMaximized(true);
+//        mainStage.setMaximized(true);
     }
 
     public static void main(String[] args) {
